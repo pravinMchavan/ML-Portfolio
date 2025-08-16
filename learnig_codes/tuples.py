2fe95b9288adbef25fcc837abefc9113b7e1a065
pravinMchavan <finalweb7887@gmail.com>
2025-08-16 21:31:21 +0530

    tuple started

---
+++ learnig_codes/tuples.py
@@ -0,0 +1,25 @@
+#tuple in python
+
+
+# creating tuples
+#my_tup = (1,2,3)
+# print(my_tup)
+# print(type(my_tup))
+
+# my_tup2 = (1, "pravin", True, 3.14)
+# print(my_tup2)
+
+# my_tup3 = 1,2,3
+# print(my_tup3)
+
+
+# tuple constructor
+# my_tup4 = tuple((1,5,9))
+# print(type(my_tup4))
+# print(my_tup4)
+
+
+#converting list into tuple
+# list1 = [1,2,3,]
+# new_tup = tuple(list1)
+# print(new_tup)
