aa3b1f491c29f71977a40e87cf285baf74f5a624
pravinMchavan <finalweb7887@gmail.com>
2025-09-03 09:14:30 +0530

    Q4 solved

---
+++ practice code/Q2_indentation.py
@@ -0,0 +1,5 @@
+# 2.	Write a function with correct indentation that prints Hello, World!
+
+def hello():
+    print("Hello, World!")
+hello()
---
+++ practice code/Q4_inlineComment.py
@@ -0,0 +1,6 @@
+# 4.	Show how to write an inline comment using # in your code.
+
+def add(a, b):  # Declaring function with parameters
+    return a + b  # Returning addition of parameters
+
+print(add(1, 2))  # Printing addition of arguments
