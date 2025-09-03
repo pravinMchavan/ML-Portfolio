82aa3add2b4e072f2d6b78d7a91ac2c6b1d2f887
pravinMchavan <finalweb7887@gmail.com>
2025-09-03 09:28:59 +0530

    Q8 solved

---
+++ practice code/Q7_printsentence.py
@@ -0,0 +1,5 @@
+# 7.	Create a function that prints a sentence explaining why Python is beginner-friendly.
+def sentence():
+    print("Python is beginner-friendly because of its simplicity and readability.")
+
+sentence()
