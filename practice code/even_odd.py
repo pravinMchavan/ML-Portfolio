b7925224ee022d387dfc29f1bc9a2aa0e3b3c70e
pravinMchavan <finalweb7887@gmail.com>
2025-09-03 08:55:04 +0530

    Q1 solved

--- practice code/even_odd.py
+++
@@ -1,7 +0,0 @@
-# Write a function is_even(number) that returns True if the number is even, else False.
-
-def is_even(num):
-    return num % 2 == 0
-print(is_even(3))
-print(is_even(10))
-
--- practice code/factorial.py
+++
@@ -1,8 +0,0 @@
-# Write a function factorial(n) that returns the factorial of a number using a loop.
-
-def factorial(n):
-    result = 1
-    for i in range(1, n +1):
-        result *= i
-    return result
-print(factorial(10))
