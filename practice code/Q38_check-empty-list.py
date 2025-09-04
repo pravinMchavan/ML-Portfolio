80e19b3a9a6be9ceefc9bf94bea865f0b0757c71
pravinMchavan <finalweb7887@gmail.com>
2025-09-04 17:27:56 +0530

    Q40 solved

---
+++ practice code/Q38_check-empty-list.py
@@ -0,0 +1,7 @@
+# 2.Check if a list is empty and print True or False.
+
+def check(lst):
+   print(not lst)
+
+check([])
+check([1,2,3])
---
+++ practice code/Q40_logical-op.py
@@ -0,0 +1,6 @@
+# 4.Use and, or, and not in logical expressions and print their results.
+
+def check():
+    print((1 and 2 > 5) or (not 1 and 2 >5))
+
+check()
