7bd71dc309821e969898eb85601d8aa45c7ae22c
pravinMchavan <finalweb7887@gmail.com>
2025-09-11 21:49:41 +0530

    Q76 solved

---
+++ practice code/Q76_IMP_unique-element.py
@@ -0,0 +1,8 @@
+# 7.	Convert a list with duplicates into a set and print unique elements.
+
+def check(list1):
+    unique_elements = set(list1)
+    print(unique_elements)
+
+
+check([1, 2, 3, 4, 1, 2, 3])
