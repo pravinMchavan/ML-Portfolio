b5e206cc83b45ed75adb1c2dc91e87df0a09e27d
pravinMchavan <finalweb7887@gmail.com>
2025-09-15 20:37:41 +0530

    Q79 to 80 solved

---
+++ practice code/Q80_remove-key.py
@@ -0,0 +1,7 @@
+# Remove a key from the dictionary and print the updated dictionary.
+
+def adding():
+    student={'name': 'Pravin', 'class': '9', 'roll no': '11'}
+    student.pop('name')
+    return student
+print(adding())
