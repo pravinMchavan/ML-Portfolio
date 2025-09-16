6e362d108665788502b3e64e5a53670de0f36330
pravinMchavan <finalweb7887@gmail.com>
2025-09-16 02:38:58 +0000

    Q81 and Q82 solved

---
+++ practice code/Q82_dict-iteratetion.py
@@ -0,0 +1,7 @@
+# Iterate over the dictionary and print all keys and values line by line.
+
+def iterate():
+    student = {'name':'pravin', 'std':'9', 'school':'ckt'}
+    for key in student:
+        print(key, ':', student[key] )
+iterate()
