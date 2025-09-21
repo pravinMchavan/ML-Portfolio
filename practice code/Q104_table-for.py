b80cf090f9e2ecf8a4ffd4f38b6bc64229d73de0
pravinMchavan <finalweb7887@gmail.com>
2025-09-21 20:47:13 +0530

    Q107 and Q 108 solved

--- practice code/Q104_table-for.py
+++ practice code/Q104_table-for.py
@@ -3,4 +3,3 @@
 num = 6
 for i in range(1, 11):
     print(f"{num} * {i} = {num * i}" )
-    
---
+++ practice code/Q107_dict-key-value-for.py
@@ -0,0 +1,5 @@
+# Loop through a dictionary and print keys and values.
+
+dict1 = {'name': 'pravin', 'class': '9th'}
+for key in dict1:
+    print(f"{key}:", dict1[key])
