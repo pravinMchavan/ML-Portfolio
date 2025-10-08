ed1a7246872da1b7d6f0f1744657a46912665b5a
Pravin Chavan <finalweb7887@gmail.com>
2025-10-08 03:17:57 +0000

    print array elements

---
+++ python_learning/practice code/Q119_array-elements.py
@@ -0,0 +1,4 @@
+# Create an array of integers and print all elements
+import array
+a = array.array([1, 2, 3, 4, 5])
+print(a)
---
+++ python_learning/practice code/Q120_append-array-ele.py
@@ -0,0 +1,6 @@
+# Append an element to an array and print the updated array.
+
+import array
+a = array.array('i', [1, 2, 3, 4, 5])
+a.append(6)
+print(a)
